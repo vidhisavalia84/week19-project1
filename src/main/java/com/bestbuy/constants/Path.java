27899b519ea0463321b69c97f54e22c08c65057e
vidhisavalia84 <vidhisavalia84@gmail.com>
2023-01-09 22:59:10 +0000

    week19-project1

---
+++ src/main/java/com/bestbuy/constants/Path.java
@@ -0,0 +1,10 @@
+package com.bestbuy.constants;
+
+public class Path {
+//this is base path for all products
+
+    public static final String PRODUCT = "/products";
+
+    /*------This is base path for stores----------*/
+    public static final String STORE = "/stores";
+}
